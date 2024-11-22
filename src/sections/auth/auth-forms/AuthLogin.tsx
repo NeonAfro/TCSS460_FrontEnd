12bf198e06fa614a8b1dc682f93a966b71ef2fcd
donghwanchung <23718894+NeonAfro@users.noreply.github.com>
2024-11-21 17:07:44 -0800

    basic yarn install

--- src/sections/auth/auth-forms/AuthLogin.tsx
+++ src/sections/auth/auth-forms/AuthLogin.tsx
@@ -60,8 +60,8 @@ export default function AuthLogin({ providers, csrfToken }: any) {
     <>
       <Formik
         initialValues={{
-          email: 'Hattie.Hyatt28@yahoo.com',
-          password: 'ads12345',
+          email: 'tonyle@uw.edu',
+          password: 'tonyle123!',
           submit: null
         }}
         validationSchema={Yup.object().shape({

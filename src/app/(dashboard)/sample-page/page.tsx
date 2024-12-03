9ea72afb5b1c95aac18fa5afaeb532aa15c94279
donghwanchung <23718894+NeonAfro@users.noreply.github.com>
2024-12-03 14:35:45 -0800

    functional ratings, complete new book

--- src/app/(dashboard)/sample-page/page.tsx
+++ src/app/(dashboard)/sample-page/page.tsx
@@ -1,12 +1,14 @@
 import SamplePage from 'views/sample-page';
 import Pagination from 'components/Pagination';
 import RightDrawer from 'views/create-new-book';
+import HoverRating from 'components/Rating';
 
 // ==============================|| PAGE ||============================== //
 
 export default function SampleViewPage() {
   return (
     <>
+      <RightDrawer />
       <SamplePage />
     </>
   )

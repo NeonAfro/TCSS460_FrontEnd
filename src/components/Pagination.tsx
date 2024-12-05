adf91b8766843f8d2c37e45e0269223fd1634665
Jsbrenio <brenio.jeremiah.2021@gmail.com>
2024-12-04 17:37:38 -0800

    Finally Fixed Pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import axios from "utils/axios";
 import Book, { IBook } from "./Book";
 import Range from "@mui/material/Pagination";
+import { round } from "lodash";
 
 const styles = {
   container: {
@@ -19,32 +20,52 @@ const styles = {
     padding: "20px",
   },
 };
-
 export default function Pagination() {
   const [data, setData] = useState<{ books: IBook[] } | null>(null);
-
-  useEffect(() => {
-    fetchBooks();
-  }, []);
-
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(30);
+  const [maxBooks, setMaxBooks] = useState(0);
+  
   const fetchBooks = async () => {
     try {
-      const response = await axios.get("/c/books/all/150/30");
+      const response = await axios.get(`/c/books/all/${page}/${limit}`);
       setData(response.data);
     } catch (error) {
       console.error("Failed to fetch books:", error);
     }
   };
 
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  useEffect(() => {
+    try {
+      axios.get(`/c/books/all/1/10000`)
+      .then((response) => {
+        const lim = response.data.books.length;
+        setMaxBooks(Math.round(lim / limit));
+      });
+    } catch (error) {
+      console.error("Failed to fetch books:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchBooks();
+  }, [page, limit]);
+
+
   return (
     <div style={styles.container}>
       <div style={styles.grid}>
         {data &&
-          data.books.map((book, i) => (
-            <Book key={i} book={book} refreshBooks={fetchBooks} />
+          data.books.map((book) => (
+            <Book key={book.isbn13} book={book} refreshBooks={fetchBooks} />
           ))}
       </div>
-      <Range count={10} color="primary" />
+
+      <Range count={maxBooks} color="primary" onChange={handleChange} />
     </div>
   );
 }
